c709934e68d2d01ba48dad70d92591f774f9bdf4
Peter Leaver <peteleaver@madetech.com>
2026-01-23 11:24:19 +0000

    add get_by_id endpoint

--- app.py
+++ app.py
@@ -11,13 +11,19 @@ def _db_connect():
 
 @app.teardown_request
 def _db_close(exc):
-    if not database.is_closed():
+    if not app.testing and not database.is_closed():
         database.close()
 
 @app.get('/coins')
 def get_all_coins():
     formatted_query = [coin for coin in Coin.select().dicts()]
     return jsonify(formatted_query)
+
+@app.get('/coins/<coin_id>')
+def get_coin_by_id(coin_id):
+    coin = Coin.get_by_id(coin_id)
+    format_coin = model_to_dict(coin)
+    return jsonify(format_coin)
 
 
 if __name__ == '__main__':
--- test_coins_api.py
+++ test_coins_api.py
@@ -2,7 +2,8 @@ import pytest
 from db import *
 from app import app
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(autouse=True)
 def setup_test_db():
     connect_to_db(app, TEST_DB)
     TEST_DB.create_tables([Coin])
@@ -13,7 +14,7 @@ def setup_test_db():
     TEST_DB.drop_tables([Coin])
     TEST_DB.close()
 
-@pytest.fixture
+@pytest.fixture(scope='function')
 def client(setup_test_db):
     app.testing = True
     with app.test_client() as client:
@@ -21,7 +22,18 @@ def client(setup_test_db):
 
 
 def test_get_all_coins(client):
-    response = client.get("/coins")
+    response = client.get('/coins')
     assert response.status_code == 200
     assert 'automate' in response.text
     assert 'automation' in response.text
+
+def test_get_coin_by_id(client):
+    coins = client.get('/coins')
+    coins_id = coins.get_json()[0]['id']
+    response = client.get(f'/coins/{coins_id}')
+    expected_id = f'{coins_id}'
+    expected_name = 'automate'
+    expected_desc = 'automation'
+    assert expected_id in response.text
+    assert expected_name in response.text
+    assert expected_desc in response.text
