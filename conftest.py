e4cd8c6fe025bd26feffca044d313d7258ec1d1f
Peter Leaver <peteleaver@madetech.com>
2026-01-26 18:03:06 +0000

    remove CoinDuty from table creation

--- conftest.py
+++ conftest.py
@@ -6,13 +6,13 @@ from app import app
 @pytest.fixture(autouse=True)
 def setup_test_db():
     connect_to_db(app, TEST_DB)
-    TEST_DB.create_tables([Coin, Duty, CoinDuty])
+    TEST_DB.create_tables([Coin, Duty])
     Coin.create(name='automate', description='automation')
     Duty.create(name='D1', description='duty 1')
     
     yield
 
-    TEST_DB.drop_tables([Coin, Duty, CoinDuty])
+    TEST_DB.drop_tables([Coin, Duty])
     TEST_DB.close()
 
 @pytest.fixture(scope='function')
