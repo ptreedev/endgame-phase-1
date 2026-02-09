e78b286ea8d544fe945711629a38de9698b1d9dd
Peter Leaver <166836879+ptreedev@users.noreply.github.com>
2026-02-09 10:56:03 +0000

    update security group

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   subnet_id              = "subnet-ba3fc8d3"
-  vpc_security_group_ids = ["sg-0213e780821464075"]
+  vpc_security_group_ids = ["sg-0c30518db572cf4f2"]
   key_name               = "terraform-key"
   user_data = templatefile("${path.module}/cloud-init.yaml.tmpl", {
     instance_name = "pete-endgame-ec2"
