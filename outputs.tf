4d69495b681ab5f78a7866de9bb617aec034a827
Peter Leaver <166836879+ptreedev@users.noreply.github.com>
2026-03-16 17:41:54 +0000

    add vpc_id and subnet_id to outputs so fronted pipeline can read them

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,11 @@ output "app_url" {
   description = "The URL to access your web application"
   value       = "http://${aws_eip.app_eip.public_ip}"
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
